55bc02f365fca44183546caefacdbe1579794b51
DanielaSanchezb <danielajulianasbay@ufps.edu.co>
2021-10-21 00:57:18 -0500

    form-actividad terminado

--- src/app/forms/form-actividad/form-actividad.component.ts
+++ src/app/forms/form-actividad/form-actividad.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActividadService } from 'src/app/services/actividad.service';
+import { TourService } from 'src/app/services/tour.service';
 
 @Component({
   selector: 'app-form-actividad',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormActividadComponent implements OnInit {
 
-  constructor() { }
+  public tours:any = [];
+  public form !: FormGroup;
+  
+ 
+  constructor(
+    private actividadService:ActividadService,
+    private tourService:TourService,
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.agregarTours();
+    this.form=this.formBuilder.group({ 
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      tour: ['', Validators.required],
+      urlImg: ['', Validators.required],
+    });
+  }
+
+  public agregarTours(){
+    this.tourService.listarTour().subscribe(tours=>{
+      this.tours = tours; 
+    })
+  }
+
+  public enviarData(){
+    this.actividadService.post(this.form.value).subscribe()
+      
   }
 
 }
--- src/app/services/actividad.service.ts
+++ src/app/services/actividad.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as global from 'global'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActividadService {
 
-  constructor() { }
+  uri =`${global.url}/actividad/`;
+  constructor(private http: HttpClient) { }
+
+  public listarActividad():Observable<any>{
+    return this.http.get<any>(this.uri)
+  }
+
+  public post(actividad:any):Observable<any>{
+    return this.http.post<any>(this.uri, actividad)
+  }
 }
